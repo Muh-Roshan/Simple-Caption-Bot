6311c4ecb1a8868c279cf964597af1acc9f3dada
Call me...leo <74614943+its-leo-bitch@users.noreply.github.com>
2021-10-17 13:48:13 +0530

    From Team XeQulsT

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+import re
+
+Class config:
+  API_ID = int(os.environ.get("API_ID", ''))
+  API_HASH = os.environ.get("API_HASH", '')
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+  EFFECT ="""
+  <i>
+  **@Stopiracy @XdGangZ** 
+  </i>
+  """
